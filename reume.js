1f1c9af297a3f8d5a501b90a88fe2dd182f40621
Suraj Yadav <surajyadavy676@gmail.com>
2024-03-14 16:21:37 +0530

    change gdrive url

--- reume.js
+++ reume.js
@@ -6,7 +6,7 @@ document.getElementById('resumeDownload').addEventListener('click', function(eve
   var resumeUrl = "images/Suraj-Yadav-Resume.pdf";
 
   // Open Google Drive link in a new tab
-  var driveUrl = 'https://drive.google.com/file/d/174uSMsICxczDCKwm1AEcXdSXnVC20JI2/view?usp=drive_link';
+  var driveUrl = 'https://drive.google.com/file/d/1p69EivAOoNjtrqUO0xAkMwjWYmbVgGP3/view?usp=drive_link';
 
   // Create an anchor element to initiate the download
   var downloadLink = document.createElement('a');
@@ -28,7 +28,7 @@ document.getElementById('download').addEventListener('click', function(event) {
   var resumeUrl = "images/Suraj-Yadav-Resume.pdf";
 
   // Open Google Drive link in a new tab
-  var driveUrl = 'https://drive.google.com/file/d/174uSMsICxczDCKwm1AEcXdSXnVC20JI2/view?usp=drive_link';
+  var driveUrl = 'https://drive.google.com/file/d/1p69EivAOoNjtrqUO0xAkMwjWYmbVgGP3/view?usp=drive_link';
 
   // Create an anchor element to initiate the download
   var downloadLink = document.createElement('a');
